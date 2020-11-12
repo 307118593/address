9de0d026e420a40191c3c1cea8cf42f92f705173
宋韬 <307118593@qq.com>
2020-11-12 16:35:53 +0800

    init

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once __DIR__.'/vendor/autoload.php';
+use Address\Test;
+
+echo 999;
+$test = new Test();
+$test->save();
---
+++ src/Test.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Address;
+class Test
+{
+    public function save()
+    {
+        echo 'composer-test';
+    }
+}
